9fef8bb91e3b2f3a57da13f493209be606a3e25f
vinicius023 <viniciusbatista023@gmail.com>
2017-02-17 09:59:06 -0200

    M - Criando metodo Vinicius

--- src/index.java
+++ src/index.java
@@ -5,11 +5,17 @@ public class index {
     public static void main(String[] args) {
 
         ronan();
+        vinicius();
     }
 
     public static void ronan(){
 
         System.out.println("Função criado por Ronan");
+    }
+
+    public static void vinicius(){
+
+        System.out.println("Gnomos voadores!!!");
     }
 }
 
